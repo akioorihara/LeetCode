42b0d6749be446d8163625181604bc3a8771d650
Akio Orihara <akioorihara@protonmail.com>
2024-02-03 23:12:36 -0600

    local errors

--- Problem217/Program.cs
+++ Problem217/Program.cs
@@ -2,7 +2,7 @@
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 Example 1:
 
-Input: nums = [1,2,3,1]
+Input: ContainsDuplicate
 Output: true
 Example 2:
 
@@ -22,7 +22,20 @@ Constraints:
 
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Calling Contains Duplicate function!");
+
+int[] nums = [1,2,3,4];
+int[] array2 = [1, 2, 3, 4, 5, 6];
+
+ContainsDuplicate(nums);
+
+bool ContainsDuplicate(int[] nums){
+
+
+    return true;
+}
+
+
 
 
 
