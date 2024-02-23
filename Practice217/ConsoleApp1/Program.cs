28cb47a56d358867bfb0ef9167cb3c26c8bf7b89
Akio Orihara <akioorihara@protonmail.com>
2024-02-22 21:36:52 -0600

    push

---
+++ Practice217/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Example 1:
+
+Input: ContainsDuplicate
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+*/ 
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calling Contains Duplicate function!");
+
+int[] nums = [1,2,3,4];
+int[] array2 = [1, 2, 3, 4, 5, 6];
+
+ContainsDuplicate(nums);
+
+bool ContainsDuplicate(int[] nums){
+
+
+    return true;
+}
+
+
+
+
+
