0c43ed129da78f2dbb193e62c6383ddff9dc955b
Akio O <akioorihara@protonmail.com>
2024-05-07 23:13:12 -0500

    Add files via upload

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,7 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//#412 - FizzBuzz 
+
+//Given an integer n, return a string array answer (1-indexed) where:
+
+//answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+//answer[i] == "Fizz" if i is divisible by 3.
+//answer[i] == "Buzz" if i is divisible by 5.
+//answer[i] == i(as a string) if none of the above conditions are true.
 
 
+//Example 1:
+//Input: n = 3
+//Output: ["1","2","Fizz"]
+
+//Example 2:
+//Input: n = 5
+//Output: ["1","2","Fizz","4","Buzz"]
+
+//Example 3:
+//Input: n = 15
+//Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+//Constraints: 1 <= n <= 104
 
 
+using FizzBuzz;
+
+
+Solutions solutions = new Solutions();
+
+//n = 3 
+var res = solutions.FizzBuzz(3);
+Console.WriteLine(res);
+
+
+Console.WriteLine("EOF");
 Console.ReadLine();
---
+++ FizzBuzz/Solutions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace FizzBuzz
+{
+	public class Solutions
+	{
+		public Solutions()
+		{
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="n"></param>
+		/// <returns></returns>
+        public IList<string> FizzBuzz(int n)
+        {
+
+
+
+
+
+			return new List<string> ();
+        }
+
+
+    }
+}
+
