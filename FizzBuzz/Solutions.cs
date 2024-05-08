aa0c94297e753f45b49676cd2b326c5cb1d455b1
Akio O <akioorihara@protonmail.com>
2024-05-07 23:19:46 -0500

    Add files via upload

--- FizzBuzz/Solutions.cs
+++ FizzBuzz/Solutions.cs
@@ -15,9 +15,26 @@ namespace FizzBuzz
 		/// <returns></returns>
         public IList<string> FizzBuzz(int n)
         {
-
-
-
+			//Constrains and validations
+			if (n <= 0)
+				return new List<string>(); //Return an empty list 
+			else if (n % 3 == 0)
+			{
+				//fizz
+			}
+			else if (n % 5 == 0)
+			{
+				//buzz
+			}
+			else if (n % 3 == 0 && n % 5 == 0)
+			{
+				//fizzbuzz
+			}
+			else
+			{
+				//i (as a string) if none of the above conditions are true
+				return new List<string> {"i"};
+			}
 
 
 			return new List<string> ();
