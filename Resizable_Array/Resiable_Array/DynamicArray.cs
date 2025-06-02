49f748bce5a859966162d559dff001483d9a4afa
Akio Orihara <akioorihara@protonmail.com>
2025-06-01 22:32:25 -0500

    ..

---
+++ Resizable_Array/Resiable_Array/DynamicArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Resiable_Array
+{
+    public class DynamicArray
+    {
+        public DynamicArray()
+        {
+
+        }
+
+        public DynamicArray(int capacity)
+        {
+
+        }
+
+        public int Get(int i)
+        {
+
+        }
+
+        public void Set(int i, int n)
+        {
+
+        }
+
+        public void PushBack(int n)
+        {
+
+        }
+
+        public int PopBack()
+        {
+
+        }
+
+        private void Resize()
+        {
+
+        }
+
+        public int GetSize()
+        {
+
+        }
+
+        public int GetCapacity()
+        {
+
+        }
+    }
+}
+
