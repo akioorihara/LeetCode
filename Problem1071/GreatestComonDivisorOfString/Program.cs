d849aebd27eefe90a24b0dd71edf4197a176e8d1
Akio Orihara <akioorihara@protonmail.com>
2025-06-16 22:15:11 -0500

    creating a boilerplate for 1071

---
+++ Problem1071/GreatestComonDivisorOfString/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+
+1071 - Greatest Common Divisor of Strings 
+
+For two strings s and t, we say t divides s " if and only if s = t + t + t 
+    (i.e. is concatenated with itself one or more times
+
+Given two strings `str1` and `str2`, return the largest string x such x divides both str1 and str2 
+
+
+Example 1:
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+
+Example 2:
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Example 3:
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+
+*/
+
+
+
+string str1 = "ABCABC", str2 = "ABC";
+
