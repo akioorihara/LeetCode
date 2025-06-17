a439980210d7d788e6b1acdf777dfa9b1f73d60e
Akio Orihara <akioorihara@protonmail.com>
2025-06-16 22:07:29 -0500

    complete

--- Problem1768/Merge_String_Alternately/Program.cs
+++ Problem1768/Merge_String_Alternately/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var word1 = "ab";
+var word2 = "pqrs";
 
+int word_len = word1.Length > word2.Length ? word1.Length : word2.Length;
+
+var result = new System.Text.StringBuilder();
+int i = 0, j = 0;
+
+while (i < word1.Length && j < word2.Length)
+{
+    result.Append(word1[i++]);
+    result.Append(word2[j++]);
+}
+
+if (i < word1.Length)
+    result.Append(word1.Substring(i));
+if (j < word2.Length)
+    result.Append(word2.Substring(j));
+
+// need to return as string()
+
+Console.WriteLine(result);
