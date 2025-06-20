adaab140cc7e9054b7e51b25e7e5fa86ef7899a0
Akio Orihara <akioorihara@protonmail.com>
2025-06-20 07:23:38 -0500

    adding some boilerplates

---
+++ ReverseString/ReverseString/Solutions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ReverseString
+{
+	public class Solutions
+	{
+            public class Solution
+        {
+            public void ReverseString(char[] s)
+            {
+                int left = 0;
+                int right = s.Length - 1;
+
+                while (left < right)
+                {
+                    // Swap s[left] and s[right]
+                    char temp = s[left];
+                    s[left] = s[right];
+                    s[right] = temp;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+
+    }
+}
+
