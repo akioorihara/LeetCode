1055a8b551e02879392da28b23e3e577a1818fd5
Akio Orihara <akioorihara@protonmail.com>
2025-06-21 08:33:30 -0700

    putting some boilerplates ready for this exercise

--- ReverseString/ReverseString/Program.cs
+++ ReverseString/ReverseString/Program.cs
@@ -1,17 +1,19 @@
-﻿//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+﻿/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
 
-//Note that you must do this in-place without making a copy of the array.
+
+Example - 1 
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example - 2 
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+*/
 
 
-
-//Example 1:
-
-//Input: nums = [0, 1, 0, 3, 12]
-//Output: [1,3,12,0,0]
-//Example 2:
-
-//Input: nums = [0]
-//Output: [0]
 
 
 
