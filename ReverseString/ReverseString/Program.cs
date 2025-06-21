ef9dbd1d377c306d78d030423f2a42365d3cd4f8
Akio Orihara <akioorihara@protonmail.com>
2025-06-21 09:10:57 -0700

    passes some test cases but not all

--- ReverseString/ReverseString/Program.cs
+++ ReverseString/ReverseString/Program.cs
@@ -4,16 +4,20 @@ You must do this by modifying the input array in-place with O(1) extra memory.
 
 
 Example - 1 
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
+Input: s = ['h','e','l','l','o']
+Output: ['o','l','l','e','h']
 
 Example - 2 
-Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
+Input: s = ['H','a','n','n','a','h']
+Output: ['h','a','n','n','a','H']
 
 */
 
 
+var solution = new Solution();
+char[] str = new char[] {'H','a','n','n','a','h'};
+
+solution.ReverseString(str);
 
 
 
@@ -21,6 +25,29 @@ public class Solution
 {
     public void ReverseString(char[] s)
     {
+        Console.WriteLine(s);
+        int lastIndex = s.Length - 1;
+        int firstIndex = 0;
+        foreach (char e in s)
+        {
+            //swap 
+            if (lastIndex == firstIndex)
+            {
+                //cannot swap + dont do anything 
+                break;
+            }
+            else
+            {
 
+                char tempChar = e;
+                s[firstIndex] = s[lastIndex];
+                s[lastIndex] = e;
+            }
+            firstIndex++;
+            lastIndex--;
+
+        }
+
+        Console.WriteLine(s);
     }
 }
