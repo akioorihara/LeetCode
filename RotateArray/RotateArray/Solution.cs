21dee9014fba8ed291a87d097af35e45e1b57b91
Akio Orihara <akioorihara@protonmail.com>
2025-06-25 18:43:49 -0500

    before looking at the solution

--- RotateArray/RotateArray/Solution.cs
+++ RotateArray/RotateArray/Solution.cs
@@ -1,6 +1,8 @@
 
 
 
+using System.Diagnostics.Contracts;
+
 public class Solution
 {
     /// <summary>
@@ -10,28 +12,36 @@ public class Solution
     /// <param name="k"></param>
     public void Rotate(int[] nums, int k)
     {
+        
+        Console.WriteLine("Input  Array: " + string.Join(",", nums));
         // error out if the input k is smaller than 0
-        if (k >= 0)
+        if (k <= 0)
             Console.WriteLine("The input K cannot be negative");
 
 
         // rotate 
         int[] rotatedArray = new int[nums.Length];
 
-        //[1,2,3,4,5,6,7] - original 
+        //                             [1,2,3,4,5,6,7] - original 
         //rotate 1 steps to the right: [7,1,2,3,4,5,6]
         //rotate 2 steps to the right: [6,7,1,2,3,4,5]
         //rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-        for (int i = 0; i < k; i++)
+        for (int i = 0; i < nums.Length; i++)
         {
-
-            
-
+            if (i == nums.Length - 1)
+            {
+                rotatedArray[0] = nums[i];
+            }
+            else
+            {
+                rotatedArray[i + 1] = nums[i];
+            }
 
 
         }
 
+        Console.WriteLine("Output Array: " + string.Join(",",rotatedArray));
 
 
     }
