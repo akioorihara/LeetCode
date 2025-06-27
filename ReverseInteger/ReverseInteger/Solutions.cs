d0f0d3a06b22180a12e1bbb770fb01b2a5574403
Akio Orihara <akioorihara@protonmail.com>
2025-06-26 22:06:43 -0500

    wip

--- ReverseInteger/ReverseInteger/Solutions.cs
+++ ReverseInteger/ReverseInteger/Solutions.cs
@@ -1,7 +1,19 @@
+using System.Text;
+
 public class Solutions
 {
     public int Reverse(int x)
     {
+        string stringCopy = x.ToString();
+        StringBuilder sb = new StringBuilder(x.ToString());
+
+        for (int i = 0; i < stringCopy.Length; i++)
+        {
+
+
+
+
+        }
 
 
         return 0;
