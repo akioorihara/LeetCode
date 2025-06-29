fe8dd3ea86b5ae237840e4b478a1ceaab8f0c690
Akio Orihara <akioorihara@protonmail.com>
2025-06-29 13:04:52 -0500

    The desc + examples set up

---
+++ BestTimeToBuyAndSell/BestTimeToBuyAndSell/Solutions.cs
@@ -0,0 +1,25 @@
+public class Solutions
+{
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="prices"></param>
+    /// <returns></returns>
+    public int MaxProfix(int[] prices)
+    {
+
+
+
+
+
+
+        return 0;
+    } 
+
+
+
+
+
+}
