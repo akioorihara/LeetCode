431a629f5793e65ff866c2178d74333d76da9b9d
Akio Orihara <akioorihara@protonmail.com>
2025-06-29 14:00:29 -0500

    completed within 10 mins or so

--- ContainsDuplicate/ContainsDuplicate/Solutions.cs
+++ ContainsDuplicate/ContainsDuplicate/Solutions.cs
@@ -1,17 +1,23 @@
+using System.Reflection.Metadata.Ecma335;
+
 public class Solutions
 {
 
     public bool ContainsDuplicate(int[] nums)
     {
 
+        Array.Sort(nums);
+        // Console.WriteLine(string.Join("",nums));
 
-
-
-
+        int nextIndex = 1;
+        for (int i = 0; i < nums.Length-1; i++)
+        {
+            if (nums[i] == nums[nextIndex])
+            {
+                return true;
+            }
+            nextIndex++;
+        }       
         return false;
     }
-
-
-
-
 }
