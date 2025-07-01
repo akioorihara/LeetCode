09f4de1d13e902504c2f99841a7362e3419aa430
Akio Orihara <akioorihara@protonmail.com>
2025-06-30 22:21:59 -0500

    solution is added

--- IntersectionOfTwoArraysll/IntersectionOfTwoArraysll/Solutions.cs
+++ IntersectionOfTwoArraysll/IntersectionOfTwoArraysll/Solutions.cs
@@ -10,28 +10,24 @@ public class Solutions
     public int[] Intersect(int[] nums1, int[] nums2)
     {
 
-        int size = nums1.Length <= nums2.Length ? nums1.Length : nums2.Length;
-        // int[] matched = new int[size];
-        List<int> mt = new List<int>();
+        
+    List<int> result = new List<int>();
+    bool[] used = new bool[nums2.Length];
 
-        int counter = 0;
-        for (int i = 0; i < nums1.Length; i++)
+    for (int i = 0; i < nums1.Length; i++)
+    {
+        for (int j = 0; j < nums2.Length; j++)
         {
-            for (int j = 0; j < nums2.Length; j++)
+            if (!used[j] && nums1[i] == nums2[j])
             {
-
-                if (nums1[i] == nums2[j])
-                {
-                    // matched[counter] = nums1[i];
-                    mt.Add(nums1[i]);
-                    counter++;
-                    break; 
-                }
+                result.Add(nums1[i]);
+                used[j] = true;  // mark as used
+                break;
             }
         }
-
-        return mt.ToArray();
     }
+
+    return result.ToArray();
 
 
 
