825f331c79eaf3b606651d548a1df71074a77284
Akio Orihara <akioorihara@protonmail.com>
2025-06-30 21:18:33 -0500

    new problem called intersect of two arrays 2

---
+++ IntersectionOfTwoArraysll/IntersectionOfTwoArraysll/Solutions.cs
@@ -0,0 +1,21 @@
+public class Solutions
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+
+
+
+        return new int[] { };
+    }
+
+
+
+
+}
