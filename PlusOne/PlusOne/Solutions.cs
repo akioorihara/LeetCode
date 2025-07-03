cc5c53877f379ccdfc6c03ba8f40680816f5ff4e
Akio Orihara <akioorihara@protonmail.com>
2025-07-02 22:38:22 -0500

    normal cases are covered and wip for the edge cases

--- PlusOne/PlusOne/Solutions.cs
+++ PlusOne/PlusOne/Solutions.cs
@@ -1,15 +1,39 @@
+using System.Data.Common;
+
 public class Solutions
 {
 
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="digits"></param>
+    /// <returns></returns>
     public int[] PlusOne(int[] digits)
     {
 
+        // One way to solve - look at the last element of the array
+        // If not 9, increment 
+        // otherwise, make it 0 and then increment the value one before 
+
+        if (digits[^1] != 9)
+        {
+            digits[^1] += 1;
+            return digits;
+        }
+
+        //increment 9(s) 
+        for (int i = digits.Length - 1; i > 0; i--)
+        {
 
 
 
 
-        return new int[] { };
+        }
+
+
+
+
+        return digits;
 
     }
 
