16e50832032dde95f0aaafbaa47aa71887a5204a
Akio Orihara <akioorihara@protonmail.com>
2025-07-04 13:30:42 -0500

    solved the problem

--- MoveZeroes/MoveZeroes/Solutions.cs
+++ MoveZeroes/MoveZeroes/Solutions.cs
@@ -9,16 +9,22 @@ public class Solution
     /// <param name="nums"></param>
     public void MoveZeroes(int[] nums)
     {
-        int pointer = 0;
+        int lastNonZeroLocation = 0;
         for (int i = 0; i < nums.Length; i++)
         {
-            
-
-
-
-
-
+            if (nums[i] != 0)
+            {
+                nums[lastNonZeroLocation] = nums[i];
+                lastNonZeroLocation++;
+            }
         }
+
+
+        for (int i = lastNonZeroLocation; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+
 
         Console.WriteLine(string.Join("",nums));
 
