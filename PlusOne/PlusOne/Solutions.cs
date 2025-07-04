8285ba67ad1352dc8ae2c1bfaa89f597f8223d82
Akio Orihara <akioorihara@protonmail.com>
2025-07-03 21:20:52 -0500

    wip

--- PlusOne/PlusOne/Solutions.cs
+++ PlusOne/PlusOne/Solutions.cs
@@ -15,6 +15,7 @@ public class Solutions
         // If not 9, increment 
         // otherwise, make it 0 and then increment the value one before 
 
+        // get the last element of array = digits[digits.Length - 1]
         if (digits[^1] != 9)
         {
             digits[^1] += 1;
