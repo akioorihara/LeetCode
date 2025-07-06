8ee34e8deedf6d671447aee2876c23958ed40d8a
Akio Orihara <akioorihara@protonmail.com>
2025-07-05 21:58:33 -0500

    new problem called first uqniue character in a string

---
+++ FirstUniqueCharinString/FirstUniqueCharinString/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+
+Problem - First Unique Charactor in a string 
+
+
+Given a string s, find the first non-repeating character in it and return its index. 
+If it does not exist, return -1.
+
+Example 1:
+Input: s = "leetcode"
+Output: 0
+Explanation:
+The character 'l' at index 0 is the first character that does not occur at any other index.
+
+Example 2:
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+Input: s = "aabb"
+Output: -1
+
+
+*/
+
+Solutions solution = new Solutions();
+
+Console.WriteLine(solution.FirstUniqChar("leetcode"));
+Console.WriteLine(solution.FirstUniqChar("loveleetcode"));
+Console.WriteLine(solution.FirstUniqChar("aabb"));
---
+++ FirstUniqueCharinString/FirstUniqueCharinString/Solutions.cs
@@ -0,0 +1,13 @@
+public class Solutions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int FirstUniqChar(string s)
+    {
+
+        return 1;
+    }
+}
