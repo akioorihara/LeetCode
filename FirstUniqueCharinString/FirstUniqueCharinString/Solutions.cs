8520f74830d37f1a5eb85c5f674244f5185c9cf9
Akio Orihara <akioorihara@protonmail.com>
2025-07-05 22:05:46 -0500

    wip

--- FirstUniqueCharinString/FirstUniqueCharinString/Solutions.cs
+++ FirstUniqueCharinString/FirstUniqueCharinString/Solutions.cs
@@ -1,12 +1,31 @@
 public class Solutions
 {
     /// <summary>
-    /// 
+    /// Given a string and return a first unique char 
+    /// If not found, return -1
     /// </summary>
     /// <param name="s"></param>
     /// <returns></returns>
     public int FirstUniqChar(string s)
     {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        int counter = 0;
+        foreach (var c in s)
+        {
+            if (dict.ContainsKey(c))
+            {
+                dict[c]++;
+            }
+            else
+            {
+                dict[c] = 1;
+            }
+
+        }
+
+        dict.Where(x => x.Value == 1);
+
 
         return 1;
     }
