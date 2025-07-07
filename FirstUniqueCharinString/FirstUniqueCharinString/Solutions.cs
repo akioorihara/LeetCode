f0e68f1b3172bf8bcb19eeb6624ee28c2170eb26
Akio Orihara <akioorihara@protonmail.com>
2025-07-06 21:06:21 -0500

    before the solution lookup

--- FirstUniqueCharinString/FirstUniqueCharinString/Solutions.cs
+++ FirstUniqueCharinString/FirstUniqueCharinString/Solutions.cs
@@ -1,3 +1,6 @@
+using System.Data.Common;
+using System.Runtime.InteropServices;
+
 public class Solutions
 {
     /// <summary>
@@ -8,36 +11,39 @@ public class Solutions
     /// <returns></returns>
     public int FirstUniqChar(string s)
     {
-        Dictionary<char, int> dict = new Dictionary<char, int>();
+        Dictionary<char, int> countUniqueChars = new Dictionary<char, int>();
+        Dictionary<char, int> countIndices = new Dictionary<char, int>();
 
-        int firstIndex = 0;
-        foreach (var c in s)
+        int index = 0;
+        for (int i = 0; i < s.Length; i++)
         {
-            if (dict.ContainsKey(c))
+            if (countUniqueChars.ContainsKey(s[i]))
             {
-                dict[c]++;
+                countUniqueChars[s[i]]++;
             }
             else
             {
-                dict[c] = 1;
-                firstIndex++;
+                countUniqueChars[s[i]] = 1;
+                countIndices[s[i]] = index;
             }
+            index++;
 
         }
 
-        dict.Where(x => x.Value == 1);
-
-
-        foreach (var kvp in dict)
+        int smallestIndex = Int32.MaxValue;
+        int smallestCandidate = 0;
+        // int uniqueSmallestCharIndex = -1;
+        foreach (var each in countUniqueChars)
         {
-            if (kvp.Value == 1)
+            if (countIndices.ContainsKey(each.Key) && each.Value == 1)
             {
-                return firstIndex;
-
+                smallestCandidate = countIndices[each.Key];
             }
+
+            if (smallestCandidate < smallestIndex)
+                smallestIndex = smallestCandidate;
         }
 
-
-        return -1;
+        return smallestIndex > 0 ? -1 : smallestIndex;
     }
 }
