f036c3b407694b240443f5dd6330b8b9874f12f3
Akio Orihara <akioorihara@protonmail.com>
2025-07-07 21:57:33 -0500

    tonight starting off the anagram problem

--- ValidAnagram/ValidAnagram/Solutions.cs
+++ ValidAnagram/ValidAnagram/Solutions.cs
@@ -8,6 +8,31 @@ public class Solutions
     /// <returns></returns>
     public bool IsAnagram(string s, string t)
     {
+        //check the length 
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        //
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            //the string s 
+            if (dict.ContainsKey(s[i]))
+            {
+                dict[s[i]]++;
+            }
+            else
+            {
+                dict[s[i]] = 1;
+            }
+        }
+
+
+        //check 
+        
 
 
         return false;
