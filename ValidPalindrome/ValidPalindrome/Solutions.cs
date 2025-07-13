ffd7aabd797fa38c25ae7cbd3cf77f15cd761407
Akio Orihara <akioorihara@protonmail.com>
2025-07-12 22:47:17 -0500

    wip and sitll being stuck

--- ValidPalindrome/ValidPalindrome/Solutions.cs
+++ ValidPalindrome/ValidPalindrome/Solutions.cs
@@ -11,8 +11,8 @@ public class Solutions
     /// <returns></returns>
     public bool IsPalindrome(string s)
     {
-        //Remove non-alphanumeric charactors 
-        // s = s.Trim();
+
+        //Remove the non-alphanumeric values 
         string cleaned = Regex.Replace(s, "[^a-zA-Z0-9]", "").ToLower();
 
         if (string.IsNullOrEmpty(cleaned))
@@ -20,12 +20,46 @@ public class Solutions
             return false;
         }
 
+        int size = s.Length;
+        if (size % 2 == 0)
+        {
+
+        }
+        else
+        {
+            //the middle letter should be removed and then iterate through
+            int middleIndex = (int)Math.Round((decimal)size / 2);
+
+            //Remove the middle index char or create a new memory to recreate without the middle value so that we can loop 
+            // s[middleIndex] = "";
+
+        }
 
 
+        Dictionary<char, int> dict = new Dictionary<char, int>();
 
+        //The first half should be added and then second half should be removed or substracted 
+        foreach (var each in s)
+        {
+            if (dict.ContainsKey(each))
+            {
+                dict[each] += 1;
+            }
+            else
+            {
+                dict[each] = 1;
+            }
 
+        }
 
         
+
+
+
+
+
+
+
 
 
 
