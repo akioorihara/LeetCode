91eeb16a2181e95631d785645496c39888a2f817
Akio Orihara <akioorihara@protonmail.com>
2025-07-12 21:07:33 -0500

    starting off the night

--- ValidPalindrome/ValidPalindrome/Solutions.cs
+++ ValidPalindrome/ValidPalindrome/Solutions.cs
@@ -2,12 +2,14 @@ public class Solutions
 {
 
     /// <summary>
-    /// 
+    /// A phrase is a palidrome if, after converting all uppercase leeters into lowercase letters and removing all non-alphanumeric chars
+    /// if reads the same forward and backwards 
     /// </summary>
     /// <param name="s"></param>
     /// <returns></returns>
     public bool IsPalindrome(string s)
     {
+        
 
 
 
