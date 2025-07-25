b014598e7f308089b3b38f76eb73d85c84552ff9
Akio Orihara <akioorihara@protonmail.com>
2025-07-24 21:45:08 -0500

    summary added

--- ImplementStrStr/ImplementStrStr/Solutions.cs
+++ ImplementStrStr/ImplementStrStr/Solutions.cs
@@ -2,7 +2,9 @@ public class Solutions
 {
 
     /// <summary>
-    /// 
+    /// Given the strings needle and haystack 
+    /// return the index of the first occurance of the needle in haystack 
+    /// if not found, return -1 
     /// </summary>
     /// <param name="haystack"></param>
     /// <param name="needle"></param>
