840cf65d6a84181c916ead7d0182b7d40f38d869
Akio Orihara <akioorihara@protonmail.com>
2025-07-24 21:44:18 -0500

    putting up the boilerplate and framework for the new problem

---
+++ ImplementStrStr/ImplementStrStr/Solutions.cs
@@ -0,0 +1,19 @@
+public class Solutions
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="haystack"></param>
+    /// <param name="needle"></param>
+    /// <returns></returns>
+    public int StrStr(string haystack, string needle)
+    {
+
+
+
+        return 0;
+    }
+
+
+}
