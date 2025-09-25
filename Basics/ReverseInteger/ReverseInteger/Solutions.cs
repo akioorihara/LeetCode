1476a9e16c1fb02b52db813996c76f351f46495a
Akio Orihara <akioorihara@protonmail.com>
2025-09-24 21:16:53 -0500

    practicing

---
+++ Basics/ReverseInteger/ReverseInteger/Solutions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ReverseInteger
+{
+	public class Solutions
+	{
+		public Solutions()
+		{
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="x"></param>
+		/// <returns></returns>
+        public int Reverse(int x)
+        {
+
+			// Checking outside of int32 ranges
+			if(x > int.MaxValue || x < int.MinValue)
+			{
+				return 0;
+			}
+
+
+
+
+
+			return -1;
+        }
+    }
+}
+
---
+++ BasicsStringBuilder/StringBuilderProjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+
+
+var sb = new StringBuilder();
+sb.Append("Hello World!");
+
+//Console.WriteLine(sb);
+
+for(int i = 0; i < 100; i++)
+{
+    sb.Append($"Line {i} : \n");
+}
+
+Console.WriteLine(sb);
