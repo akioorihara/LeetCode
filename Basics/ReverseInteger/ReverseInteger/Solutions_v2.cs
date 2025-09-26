94843e761bec1d180ec6a307ae56467a9f12a932
Akio Orihara <akioorihara@protonmail.com>
2025-09-26 05:42:12 -0500

    redo

---
+++ Basics/ReverseInteger/ReverseInteger/Solutions_v2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ReverseInteger
+{
+	public class Solutions_v2
+	{
+		public Solutions_v2()
+		{
+		}
+
+
+		/// <summary>
+		/// Reverse an integer 
+		/// </summary>
+		/// <param name="x"></param>
+		/// <returns></returns>
+		public int Reverse(int x)
+		{
+			if(x > int.MaxValue || x < int.MinValue)
+			{
+				return 0;
+			}
+
+			bool isNegative = false;
+			if(x < 0)
+			{
+				isNegative = true;
+			}
+
+			char[] arr = x.ToString().ToCharArray();
+			arr.Reverse();
+			string reverse = arr.ToString();
+
+
+			if(int.TryParse(reverse, out int result))
+			{
+                return isNegative ? -result : result;
+            }
+
+			return 0;
+        }
+
+
+	}
+}
+
