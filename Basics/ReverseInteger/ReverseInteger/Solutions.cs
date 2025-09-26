5a636b580ad2695fa1bbbda445bedf32ed40e32e
Akio Orihara <akioorihara@protonmail.com>
2025-09-25 21:45:42 -0500

    solution added

--- Basics/ReverseInteger/ReverseInteger/Solutions.cs
+++ Basics/ReverseInteger/ReverseInteger/Solutions.cs
@@ -16,37 +16,54 @@ namespace ReverseInteger
         public int Reverse(int x)
         {
 
-			// Checking outside of int32 ranges
-			if(x > int.MaxValue || x < int.MinValue)
-			{
-				return 0;
-			}
-
-			var convertedToString = x.ToString();
-			var sb = new StringBuilder();
-
-			foreach(var each in convertedToString.Reverse())
-			{
-				sb.Append(each);
-				Console.WriteLine(each);
-			}
-
-			var sbString = sb.ToString().Trim();
-
-            int result = -1;
-
-            try
+            // Checking outside of int32 ranges
+            if (x > int.MaxValue || x < int.MinValue)
             {
-				// do try parse
-				int.TryParse(sbString, out result);
-			}
-			catch(Exception ex)
-			{
+                return 0;
+            }
 
-			}
+            var convertedToString = x.ToString();
+            var sb = new StringBuilder();
+
+            // check if negavie or not
+            bool isNegative = false;
+            if (x < 0)
+            {
+                isNegative = true;
+                convertedToString = convertedToString.Substring(1);
+            }
+
+            foreach (var each in convertedToString.Reverse())
+            {
+                sb.Append(each);
+                //Console.WriteLine(each);
+            }
+
+            var sbString = sb.ToString().Trim();
 
 
-			return result;
+            // do try parse
+            if(int.TryParse(sbString, out int result))
+            {
+                return isNegative ? -result : result;
+            }
+
+            return 0;
+
+
+            //bool isNegative = x < 0;
+            //string s = Math.Abs((long)x).ToString(); // use long to avoid issues on -2147483648
+            //char[] arr = s.ToCharArray();
+            //Array.Reverse(arr);
+            //string reversed = new string(arr);
+
+            //if (int.TryParse(reversed, out int result))
+            //{
+            //    return isNegative ? -result : result;
+            //}
+            //return 0; // overflow case
+
+
         }
     }
 }
