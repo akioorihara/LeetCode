61c827b7df43109959030451fdfbb5a8a775f1d7
Akio Orihara <akioorihara@protonmail.com>
2025-09-26 05:48:50 -0500

    palindrome number boilerplate constructed

---
+++ PalindromeNumber/PalindromeNumber/Solutions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PalindromeNumber
+{
+	public class Solutions
+	{
+		public Solutions()
+		{
+		}
+
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="x"></param>
+		/// <returns></returns>
+        public bool IsPalindrome(int x)
+        {
+
+
+
+
+
+			return false;
+        }
+
+    }
+}
+
