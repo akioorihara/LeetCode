c748fca2aaafecd46d3c2929eeef54a5d8a06b19
Akio Orihara <akioorihara@protonmail.com>
2025-09-27 10:03:49 -0500

    trying to do a different way

---
+++ PalindromeNumber/PalindromeNumber/Solutions_v2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PalindromeNumber
+{
+	public class Solutions_v2
+	{
+		public Solutions_v2()
+		{
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="x"></param>
+		/// <returns></returns>
+		public bool Reverse(int x)
+		{
+
+
+
+
+			return true;
+		}
+
+	}
+}
+
